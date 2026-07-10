65c2df44097d617549beba3615f495586a075d8b
Shavez <162662217+mathsvisualization@users.noreply.github.com>
2026-07-10 13:32:17 +0000

    f

--- test.py
+++ test.py
@@ -2,6 +2,6 @@ from manimlib import *
 
 class MKC(InteractiveScene):
     def construct(self):
-        t = Text("Hello Sex")
+        t = Text("Hello Sex").set_color(BLACK)
         vmobject_to_svg(t, "s")
         self.add(t)
