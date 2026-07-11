e982aea5f23b19d2ad56363c3dac0836cbbfa8a3
Shavez <162662217+mathsvisualization@users.noreply.github.com>
2026-07-11 04:03:12 +0000

    f

--- test.py
+++ test.py
@@ -7,3 +7,8 @@ class Paid(InteractiveScene):
         logo.set_color(PURPLE_A)
         logo.to_edge(UL, buff=MED_SMALL_BUFF)
         self.add(logo)
+
+        v_line = Line(UP, RIGHT)
+        v_line.set_height(logo.get_height() * 1.5)
+        v_line.next_to(logo, RIGHT, buff=SMALL_BUFF)
+        self.add(v_line)
