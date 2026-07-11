8a426998c827604c2bcc9ea8c35c9482d35acbe0
Shavez <162662217+mathsvisualization@users.noreply.github.com>
2026-07-11 04:04:18 +0000

    f

--- test.py
+++ test.py
@@ -8,7 +8,9 @@ class Paid(InteractiveScene):
         logo.to_edge(UL, buff=MED_SMALL_BUFF)
         self.add(logo)
 
-        v_line = Line(UP, RIGHT)
+        v_line = Line(UP, DOWN)
         v_line.set_height(logo.get_height() * 1.5)
         v_line.next_to(logo, RIGHT, buff=SMALL_BUFF)
+        v_line.insert_n_curves(10)
+        v_line.set_stroke(width=[0.5, 1.5, 1.5, 0.5])
         self.add(v_line)
