542a972c9a30e550fc1aa6224fbab2fb03511a58
Shavez <162662217+mathsvisualization@users.noreply.github.com>
2026-07-11 03:52:47 +0000

    f

--- test.py
+++ test.py
@@ -3,5 +3,7 @@ from manimlib import *
 class Paid(InteractiveScene):
     def construct(self):
         logo = Tex(R"\langle \psi \rangle")
+        logo.scale(0.5)
         logo.set_color(PURPLE_A)
+        logo.to_edge(UL, buff=MED_SMALL_BUFF)
         self.add(logo)
