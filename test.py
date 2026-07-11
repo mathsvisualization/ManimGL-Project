eee7bbdab8fa16d1ab8a63ca3da1794a45580611
Shavez <162662217+mathsvisualization@users.noreply.github.com>
2026-07-11 03:34:58 +0000

    f

--- test.py
+++ test.py
@@ -2,6 +2,6 @@ from manimlib import *
 
 class MKC(InteractiveScene):
     def construct(self):
-        t = Square().set_color(BLACK).set_fill(GREEN, 0.5)
+        t = Square().set_color(BLACK).set_color([GREEN, RED], opacity=0.5)
         vmobject_to_svg(t, "s.svg")
         self.add(t)
