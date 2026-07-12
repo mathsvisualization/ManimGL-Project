1392c466ad8fd1de9cdd2855621587fa10d2803a
Shavez <162662217+mathsvisualization@users.noreply.github.com>
2026-07-12 02:05:48 +0000

    f

--- test.py
+++ test.py
@@ -64,7 +64,7 @@ class Paid(InteractiveScene):
             researcher, or simply curious about science,\\
             this collection brings thousands of carefully\\
             organized educational resources together in one place\\
-            """
+            """, font_size=26, alignment=R"\flushleft"
         )
         cont2.next_to(itm, DOWN, buff=MED_LARGE_BUFF, aligned_edge=LEFT)
         self.add(cont2)
