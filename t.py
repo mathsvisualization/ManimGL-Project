e07e5e90f06b0f30755efe08a099a85aa8d94a60
Shavez <162662217+mathsvisualization@users.noreply.github.com>
2026-08-12 08:55:20 +0000

    f

--- t.py
+++ t.py
@@ -2,6 +2,6 @@ from manimlib import *
 
 class Test(InteractiveScene):
     def construct(self):
-        eq = TexText("e^{i\\pi} = -1", font_size=56)
+        eq = Tex("e^{i\\pi} = -1", font_size=56)
         self.play(Write(eq), run_time=3)
         self.wait(2)
