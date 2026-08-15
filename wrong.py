74ccd6a1cc2af3d33f06e1613fea794424842498
Shavez <162662217+mathsvisualization@users.noreply.github.com>
2026-08-15 17:34:11 +0000

    f

--- wrong.py
+++ wrong.py
@@ -6,15 +6,18 @@ class WhatDidYouSee(InteractiveScene):
         what.shift(UP * 2)
 
         equ = Tex(R"\int x dx = \frac{x^2}{2} - C", font_size=40).set_color([PURPLE_A, PURPLE_C])
-        equ_c = equ.copy()[:-1]
+        equ_c = equ.copy()[:-2]
 
         self.play(
             Write(equ_c),
             FadeIn(what, UP * 0.5)
         )
-        self.wait()
+        self.wait(3)
         self.play(
-            Transform(equ_c, equ),
-            run_time=2
+            TransformMatchingTex(
+                equ_c, equ,
+                key_map={"-": " "}
+            ),
+            run_time=3
         )
-        self.wait()
+        self.wait(2)
