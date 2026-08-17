4d90d4533f2a1c6f0334c23999af93d1ef61c11b
Shavez <162662217+mathsvisualization@users.noreply.github.com>
2026-08-17 09:57:08 +0000

    f

--- wrong.py
+++ wrong.py
@@ -5,23 +5,36 @@ class WhatDidYouSee(InteractiveScene):
         what = TexText("Nothing wrong here,", font_size=40)
         what.shift(UP * 3)
 
-        see = TexText("What did you see?", font_size=38).set_color([YELLOW, ORANGE])
+        see = TexText("What did you see?", font_size=38)
+        see.set_color([YELLOW, ORANGE])
         see.next_to(what, DOWN, buff=MED_LARGE_BUFF)
 
-        equ = Tex(R"\int x\, dx = \frac{x^2}{2}", font_size=40).set_color([PURPLE_A, PURPLE_C])
-        equ_c = Tex(R"\int x\, dx = \frac{x^2}{2} - C", font_size=40).match_style(equ)
+        equ = Tex(
+            R"\int x\, dx = \frac{x^2}{2}",
+            font_size=40
+        ).set_color([PURPLE_A, PURPLE_C])
+
+        equ_c = Tex(
+            R"\int x\, dx = \frac{x^2}{2} - C",
+            font_size=40
+        ).match_style(equ)
+
+        minus_c = equ_c["- C"]
+        main_part = equ_c[:-2]
+
+        minus_c.set_opacity(0)
 
         self.play(
             Write(equ),
             FadeIn(what, UP * 0.5)
         )
         self.wait(3)
+
         self.play(
-            Transform(
-                equ, equ_c[:-2],
-            ),
-            FadeIn(equ_c["- C"]),
+            Transform(equ, main_part),
+            FadeIn(minus_c),
             FadeIn(see),
             run_time=1.5
         )
+
         self.wait(3)
