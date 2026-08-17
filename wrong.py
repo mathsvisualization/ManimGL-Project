4297b6d46cef7dd67c4fcf76f41cc6809d1205c7
Shavez <162662217+mathsvisualization@users.noreply.github.com>
2026-08-17 10:24:08 +0000

    f

--- wrong.py
+++ wrong.py
@@ -2,6 +2,7 @@ from manimlib import *
 
 class WhatDidYouSee(InteractiveScene):
     def construct(self):
+        color = [PURPLE_A, PURPLE_C]
         what = TexText("Nothing wrong here,", font_size=40)
         what.shift(UP * 3)
 
@@ -12,24 +13,25 @@ class WhatDidYouSee(InteractiveScene):
         equ = Tex(
             R"\int x\, dx = \frac{x^2}{2}",
             font_size=40
-        ).set_color([PURPLE_A, PURPLE_C])
+        ).set_color(color)
 
         equ_c = Tex(
             R"\int x\, dx = \frac{x^2}{2} - C",
             font_size=40
-        ).match_style(equ)
+        ).match_color(equ)
 
-        minus_c = equ_c["- C"]
+        minus_c = equ_c["- C"][0]
         main_part = equ_c[:-2]
 
         self.play(
             Write(equ),
             FadeIn(what, UP * 0.5)
         )
-        self.wait(3)
+        self.wait(1)
 
         self.play(
             Transform(equ, main_part),
+            FadeIn(minus_c),
             FadeIn(see),
             run_time=1.5
         )
