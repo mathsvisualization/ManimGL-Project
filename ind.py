17eda06fc246249889829b85a45dbfb85797ca2d
Shavez <162662217+mathsvisualization@users.noreply.github.com>
2026-08-18 12:12:40 +0000

    f

--- ind.py
+++ ind.py
@@ -86,7 +86,7 @@ class Identity(InteractiveScene):
         )
         self.wait()
 
-        frac_equ = Tex(R"\frac{6^x}{5^x} = \frac{5^5}{6^5}")
+        frac_equ = Tex(R"\frac{6^x}{5^x} = \frac{5^5}{6^5}", isolate=["6^x"])
         frac_equ.set_color(color)
         self.play(
             Transform(new_equ["6^x"][0], frac_equ["6^x"][0])
