919fc67b7d5d71da33c71d796f4e7fff82c79d9c
Shavez <162662217+mathsvisualization@users.noreply.github.com>
2026-08-19 03:28:27 +0000

    f

--- ind.py
+++ ind.py
@@ -127,7 +127,7 @@ class Identity(InteractiveScene):
             ShowCreation(frac_igp_rect)
         )
         self.wait()
-        frac_igp_rect.shift(RIGHT)
+        frac_igp_rect.shift(0.0001*RIGHT)
         self.play(ShowCreation(frac_rects[0]))
         """self.play(
             LaggedStart(
