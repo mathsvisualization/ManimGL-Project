adfc818741d0255f589846f13821cc01e2d40739
Shavez <162662217+mathsvisualization@users.noreply.github.com>
2026-09-23 10:46:45 +0000

    f

--- manim/manimlib/scene/scene.py
+++ manim/manimlib/scene/scene.py
@@ -650,12 +650,15 @@ class Scene(object):
         end_time: float | None = None,
         fade_in: float = 0.0,
         fade_out: float = 0.0,
-        repeat: int = 1
+        repeat: int = 1,
+        pan: float | None = None,
+        pan_start: float | None = None,
+        pan_end: float | None = None
     ):
         if self.skip_animations:
             return
         time = self.get_time() + time_offset
-        self.file_writer.add_sound(sound_file, time, gain, gain_to_background, start_time, end_time, fade_in, fade_out, repeat)
+        self.file_writer.add_sound(sound_file, time, gain, gain_to_background, start_time, end_time, fade_in, fade_out, repeat, pan, pan_start, pan_end)
 
     # Helpers for interactive development
 
