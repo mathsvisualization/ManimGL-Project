5a2018c4ed6b224219e7b95d8c230f88f29bf8fa
Shavez <162662217+mathsvisualization@users.noreply.github.com>
2026-09-24 03:15:22 +0000

    f

--- ob1.py
+++ ob1.py
@@ -135,7 +135,7 @@ class EnhancedDopplerEffect(Scene):
         waves.add_updater(update_waves)
 
         # 7. Add Sync Audio (If generated)
-        self.add_sound("doppler_sound.wav", start_time=4.0, end_time=2.0, repeat=4)
+        self.add_sound("doppler_sound.wav", pan=-1.0)
 
         # 8. Animate Everything via the Master Clock
         self.play(
